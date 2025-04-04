44389d86da8837324622831aedca20c929db07ff
Aayush Bhattarai <aayushbhattaraia6@gmail.com>
2025-04-04 21:01:57 +0545

    completed frontend doctorlisting page

--- admin/src/pages/admin/DoctorList.jsx
+++ admin/src/pages/admin/DoctorList.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import axios from "axios"
+import React from 'react'
 import { useSelector } from "react-redux"
-import { toast } from 'react-toastify';
 
 const DoctorList = () => {
 
@@ -10,9 +8,24 @@ const DoctorList = () => {
 
   return doctors.length > 0 ? (
     <div>
-      {doctors.map((item, index) => {
-        return <div key={index}>Helllo</div>
-      })}
+      <h2 className='text-xl mb-5'>All Doctors</h2>
+      <div className='grid grid-cols-3 space-y-4'>
+        {doctors.map((item, index) => {
+          return <div className='cursor-pointer group rounded-lg border border-gray-300 shadow-md w-[20vw] h-[55vh]' key={index}>
+            <div className=' w-full bg-blue-100 group-hover:bg-blue-200 duration-400 h-[60%] mx-auto'>
+              <img className='w-[75%] mx-auto' src={item.image} alt="" />
+            </div>
+            <div className='p-3 text-gray-700'>
+              <p className='text-lg font-medium'>{item.name}</p>
+              <p className='text-sm font-medium'>{item.speciality}</p>
+            </div>
+            <div className='text-gray-700 p-3 pt-0 flex items-center gap-3'>
+              <input className='cursor-pointer' type="checkbox" checked={item.available} />
+              <p>Available</p>
+            </div>
+          </div>
+        })}
+      </div>
     </div>
   ) : (
     <div>Loading...</div>
